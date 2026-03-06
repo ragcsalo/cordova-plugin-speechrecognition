a338fe355364f0ca008d7f449bc77b0811eb632b
Bence Kovacs <kov.benc@gmail.com>
2026-03-06 15:51:42 +0100

    Update speechRecognition.js

--- www/speechRecognition.js
+++ www/speechRecognition.js
@@ -1,27 +1,30 @@
 cordova.define("cordova-plugin-speechrecognition.SpeechRecognition", function(require, exports, module) {
-module.exports = {
-  isRecognitionAvailable: function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'isRecognitionAvailable', []);
-  },
-  startListening: function(successCallback, errorCallback, options) {
-    options = options || {};
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'startListening', [ options.language, options.matches, options.prompt, options.showPartial, options.showPopup ]);
-  },
-  stopListening: function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'stopListening', []);
-  },
-  restoreVolume: function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'restoreVolume', []);
-  },
-  getSupportedLanguages: function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'getSupportedLanguages', []);
-  },
-  hasPermission: function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'hasPermission', []);
-  },
-  requestPermission: function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'requestPermission', []);
-  }
-};
-
+  module.exports = {
+    isRecognitionAvailable: function(successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'isRecognitionAvailable', []);
+    },
+    startListening: function(successCallback, errorCallback, options) {
+      options = options || {};
+      cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'startListening', [
+        options.language || 'en-US',
+        options.matches || 5,
+        options.prompt || '',
+        options.showPartial || false,
+        options.showPopup || false,
+        options.safetyTimeout || 0
+      ]);
+    },
+    stopListening: function(successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'stopListening', []);
+    },
+    getSupportedLanguages: function(successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'getSupportedLanguages', []);
+    },
+    hasPermission: function(successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'hasPermission', []);
+    },
+    requestPermission: function(successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, 'SpeechRecognition', 'requestPermission', []);
+    }
+  };
 });
